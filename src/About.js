60601ecd523c597e04560cf44cd7bfa7ef15f427
John-hackx <myofficialsat@gmail.com>
2025-03-08 10:24:26 +0000

    updated with mobile view

--- src/About.js
+++ src/About.js
@@ -13,12 +13,6 @@ export default function App() {
   const [rating, setRating] = useState(0);
   const [movieDetails, setMovieDetails] = useState(null);
   const [isLoading, setIsLoading] = useState(false);
-  const [isWatchedOpen, setIsWatchedOpen] = useState(false);
-
-  const windowWidth = window.innerWidth;
-  // if (windowWidth <= 810) {
-  //   setIsMobileView(true);
-  // }
 
   useEffect(
     function () {
@@ -52,99 +46,49 @@ export default function App() {
     },
     [query]
   );
-  console.log(watchedMovies);
+  // console.log(watchedMovies);
   // console.log(selectedMovieID);
   return (
     <div className="app-container">
       <Navbar movies={movies} query={query} setQuery={setQuery} />
       <Main>
-        {windowWidth <= 810 && (
-          <ViewWatched
-            watchedMovies={watchedMovies}
-            isWatchedOpen={isWatchedOpen}
-            setIsWatchedOpen={setIsWatchedOpen}
-          />
-        )}
         <Box>
-          {windowWidth <= 810 ? (
-            !movieDetails ? (
-              //watched movies not opened
-              !isWatchedOpen && (
-                <>
-                  <BtnControllDisplay
-                    classname={"btn-controllDisplay"}
-                    content="-"
-                  />
-
-                  <MovieList
-                    setIsLoading={setIsLoading}
-                    setMovieDetails={setMovieDetails}
-                    selectedMovieID={selectedMovieID}
-                    setSelectedMovieID={setSelectedMovieID}
-                    setRating={setRating}
-                    selectedMovie={selectedMovie}
-                    setSelectedMovie={setSelectedMovie}
-                    movies={movies}
-                  />
-                </>
-              )
-            ) : (
-              //watched movies not opened
-              !isWatchedOpen && (
-                <>
-                  <BtnControllDisplay
-                    classname={"btn-controllDisplay"}
-                    content="-"
-                  />
-
-                  <MovieDetails
-                    isLoading={isLoading}
-                    setIsLoading={setIsLoading}
-                    setSelectedMovieID={setSelectedMovieID}
-                    selectedMovieID={selectedMovieID}
-                    setSelectedMovie={setSelectedMovie}
-                    key={selectedMovie?.imdbID}
-                    watchedMovies={watchedMovies}
-                    setWatchedMovies={setWatchedMovies}
-                    selectedMovie={selectedMovie}
-                    rating={rating}
-                    setRating={setRating}
-                    setMovieDetails={setMovieDetails}
-                    movieDetails={movieDetails}
-                  />
-                  {/* {movieDetails ? (
-                ) : (
-                  <WatchList
-                    movieDetails={movieDetails}
-                    raiting={rating}
-                    watchedMovies={watchedMovies}
-                    setWatchedMovies={setWatchedMovies}
-                  />
-                )} */}
-                </>
-              )
-            )
+          <BtnControllDisplay classname={"btn-controllDisplay"} content="-" />
+          {!isLoading ? (
+            <MovieList
+              setIsLoading={setIsLoading}
+              setMovieDetails={setMovieDetails}
+              selectedMovieID={selectedMovieID}
+              setSelectedMovieID={setSelectedMovieID}
+              setRating={setRating}
+              selectedMovie={selectedMovie}
+              setSelectedMovie={setSelectedMovie}
+              movies={movies}
+            />
           ) : (
-            //Not mobile view
-            <>
-              <BtnControllDisplay
-                classname={"btn-controllDisplay"}
-                content="-"
-              />
-
-              <MovieList
-                setIsLoading={setIsLoading}
-                setMovieDetails={setMovieDetails}
-                selectedMovieID={selectedMovieID}
-                setSelectedMovieID={setSelectedMovieID}
-                setRating={setRating}
-                selectedMovie={selectedMovie}
-                setSelectedMovie={setSelectedMovie}
-                movies={movies}
-              />
-            </>
+            <Loading />
           )}
-          {isWatchedOpen && (
+        </Box>
+        <Box>
+          <BtnControllDisplay classname={"btn-controllDisplay"} content="-" />
+          {/* {isLoading && <Loading />} */}
+          {movieDetails ? (
+            <MovieDetails
+              isLoading={isLoading}
+              setIsLoading={setIsLoading}
+              setSelectedMovieID={setSelectedMovieID}
+              selectedMovieID={selectedMovieID}
+              setSelectedMovie={setSelectedMovie}
+              key={selectedMovie?.imdbID}
+              watchedMovies={watchedMovies}
+              setWatchedMovies={setWatchedMovies}
+              selectedMovie={selectedMovie}
+              rating={rating}
+              setRating={setRating}
+              setMovieDetails={setMovieDetails}
+              movieDetails={movieDetails}
+            />
+          ) : (
             <WatchList
               movieDetails={movieDetails}
               raiting={rating}
@@ -153,60 +97,7 @@ export default function App() {
             />
           )}
         </Box>
-        {!(windowWidth <= 810) && (
-          <Box>
-            <BtnControllDisplay classname={"btn-controllDisplay"} content="-" />
-
-            {movieDetails ? (
-              <MovieDetails
-                isLoading={isLoading}
-                setIsLoading={setIsLoading}
-                setSelectedMovieID={setSelectedMovieID}
-                selectedMovieID={selectedMovieID}
-                setSelectedMovie={setSelectedMovie}
-                key={selectedMovie?.imdbID}
-                watchedMovies={watchedMovies}
-                setWatchedMovies={setWatchedMovies}
-                selectedMovie={selectedMovie}
-                rating={rating}
-                setRating={setRating}
-                setMovieDetails={setMovieDetails}
-                movieDetails={movieDetails}
-              />
-            ) : (
-              <WatchList
-                movieDetails={movieDetails}
-                raiting={rating}
-                watchedMovies={watchedMovies}
-                setWatchedMovies={setWatchedMovies}
-              />
-            )}
-          </Box>
-        )}
       </Main>
-    </div>
-  );
-}
-
-function ViewWatched({ watchedMovies, setIsWatchedOpen, isWatchedOpen }) {
-  const handleViewWatched = () => {
-    setIsWatchedOpen(!isWatchedOpen);
-  };
-
-  return (
-    <div className="view-watched" role="button" onClick={handleViewWatched}>
-      <span>
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          height="24px"
-          viewBox="0 -960 960 960"
-          width="24px"
-          fill="currentColor"
-        >
-          <path d="M280-600v-80h560v80H280Zm0 160v-80h560v80H280Zm0 160v-80h560v80H280ZM160-600q-17 0-28.5-11.5T120-640q0-17 11.5-28.5T160-680q17 0 28.5 11.5T200-640q0 17-11.5 28.5T160-600Zm0 160q-17 0-28.5-11.5T120-480q0-17 11.5-28.5T160-520q17 0 28.5 11.5T200-480q0 17-11.5 28.5T160-440Zm0 160q-17 0-28.5-11.5T120-320q0-17 11.5-28.5T160-360q17 0 28.5 11.5T200-320q0 17-11.5 28.5T160-280Z" />
-        </svg>
-      </span>
-      <p>Watched</p>
     </div>
   );
 }
@@ -391,7 +282,7 @@ function MovieCard({
       <div className="movie-image">
         <img src={image} alt="movie" />
       </div>
-
+      \
       <div className="movie-info">
         <h3>{title}</h3>
         {children}
@@ -542,22 +433,22 @@ function WatchedMovieCard({ image, title, children }) {
 }
 
 function WatchList({ movieDetails, watchedMovies, rating, setWatchedMovies }) {
-  const sumOfRating = watchedMovies?.reduce((acc, cur) => acc + cur.rating, 0);
-  const averageRating = sumOfRating / watchedMovies?.length;
+  const sumOfRating = watchedMovies.reduce((acc, cur) => acc + cur.rating, 0);
+  const averageRating = sumOfRating / watchedMovies.length;
 
-  const sumOfImdbRating = watchedMovies?.reduce(
+  const sumOfImdbRating = watchedMovies.reduce(
     (acc, cur) => acc + Number(cur.imdbRating),
     0
   );
-  const averageImdbRating = sumOfImdbRating / watchedMovies?.length;
+  const averageImdbRating = sumOfImdbRating / watchedMovies.length;
 
   const averageRuntime =
-    watchedMovies?.reduce((acc, cur) => acc + parseInt(cur.Runtime), 0) /
-    watchedMovies?.length;
+    watchedMovies.reduce((acc, cur) => acc + parseInt(cur.Runtime), 0) /
+    watchedMovies.length;
   // console.log(averageRuntime);
 
   const handleDeleteMovie = (id) => {
-    const filteredArray = watchedMovies?.filter((movie) => movie.imdbID !== id);
+    const filteredArray = watchedMovies.filter((movie) => movie.imdbID !== id);
     setWatchedMovies(filteredArray);
     console.log(id);
   };
@@ -569,27 +460,27 @@ function WatchList({ movieDetails, watchedMovies, rating, setWatchedMovies }) {
         <ul className="watched-header-list">
           <li>
             <span>#️⃣</span>
-            {watchedMovies?.length}{" "}
-            {watchedMovies?.length < 2 && watchedMovies?.length !== 0
+            {watchedMovies.length}{" "}
+            {watchedMovies.length < 2 && watchedMovies.length !== 0
               ? "movie"
               : "movies"}
           </li>
           <li>
             <span>⭐</span>
-            {watchedMovies?.length > 0 ? averageRating.toFixed(2) : "0"}
+            {watchedMovies.length > 0 ? averageRating.toFixed(2) : "0"}
           </li>
           <li>
             <span>🌟</span>
-            {watchedMovies?.length > 0 ? averageImdbRating.toFixed(2) : "0"}
+            {watchedMovies.length > 0 ? averageImdbRating.toFixed(2) : "0"}
           </li>
           <li>
             <span>🕑</span>
-            {watchedMovies?.length > 0 ? averageRuntime : "0"} mins
+            {watchedMovies.length > 0 ? averageRuntime : "0"} mins
           </li>
         </ul>
       </div>
       <div className="watched-list">
-        {watchedMovies?.map((movie) => (
+        {watchedMovies.map((movie) => (
           <WatchedMovieCard
             key={movie?.imdbID}
             title={movie.Title}
